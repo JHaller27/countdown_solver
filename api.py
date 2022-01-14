1605022993f03d4c32ed7cb3d091ae25ebe91d9b
James <jameshaller27@gmail.com>
2022-01-13 23:22:28 -0800

    Use pydantic models

--- api.py
+++ api.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI, Query, HTTPException, status
 import solver
+import pydantic
+
+
+class NumbersResponse(pydantic.BaseModel):
+    expression: str
+    result: int
+    delta: int
 
 
 app = FastAPI(title="Countdown solver")
@@ -19,8 +26,8 @@ def filter_best_only(results: list[solver.Result]) -> list[solver.Result]:
     return result_dict[min_diff]
 
 
-@app.get("/", response_model=list[solver.Result])
-def solve(target: int, numbers: list[int] = Query(...), best_only: bool = True) -> list[solver.Result]:
+@app.get("/", response_model=list[NumbersResponse])
+def solve(target: int = Query(...), numbers: list[int] = Query(...), best_only: bool = True) -> list[NumbersResponse]:
     if len(numbers) > 6:
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="May pass no more than 6 numbers")
 
@@ -30,5 +37,7 @@ def solve(target: int, numbers: list[int] = Query(...), best_only: bool = True) 
 
     if best_only:
         results = filter_best_only(results)
+
+    results = [NumbersResponse(expression=" ".join(map(str, r.operations)), result=r.result, delta=r.delta) for r in results]
 
     return results
